3b6ded8bff7175ce07353b3976fe233a90902b3a
Emilie Corpuz <emiliecorpuz@gmail.com>
2015-03-27 09:26:44 -0700

    first css

---
+++ js/MainController.js
@@ -0,0 +1,10 @@
+(function (){
+
+	angular
+		.module("ttcApp", [])
+		.controller("mainController", mainController);
+
+
+})();
+
+
---
+++ js/app.js
@@ -0,0 +1,18 @@
+(function(){
+
+angular
+	.module('ttcApp',[]);
+
+
+
+})();
+
+
+//player clicks on container, adds x or y
+
+//3 rows win,
+//3 columns win,
+//corner middle corner,
+
+
+///
