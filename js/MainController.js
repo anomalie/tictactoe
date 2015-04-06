64c3b475050f7c1e9240eaa91d80f95108921a99
Emilie Corpuz <emiliecorpuz@gmail.com>
2015-04-05 19:18:47 -0700

    Adjust Player Win and Tie CSS

--- js/MainController.js
+++ js/MainController.js
@@ -7,6 +7,7 @@
 
 				//SET CONTROLLER FUNCTION & VARIABLES//
 		function mainController($firebaseObject,$firebaseArray){
+
 			var introSound = new Audio ("./audio/intro.mp3");
 			var submitSound = new Audio ("./audio/ttc_01.mp3");
 			var winSound = new Audio ("./audio/ending.mp3");
@@ -18,17 +19,15 @@
 				"./audio/ttc_10.mp3","./audio/ttc_11.mp3","./audio/ttc_12.mp3",
 				"./audio/ttc_13.mp3","./audio/ttc_14.mp3","./audio/ttc_15.mp3",
 				"./audio/ttc_16.mp3","./audio/ttc_17.mp3","./audio/ttc_18.mp3",
-				"./audio/ttc_19.mp3"
-			];
+				"./audio/ttc_19.mp3"];
 			var ref= new Firebase("https://tictactoefbapp.firebaseio.com/");
 			var game=$firebaseObject(ref);
 			var self = this;
 				this.createGame = createGame();
-				self.enterName = 1;
 				self.game= game;
 				self.ref= ref;
 				self.playerMove = playerMove;
-				self.currentPlayer = 1 ;
+				self.currentPlayer = 0 ;
 				self.selectPlayer = selectTurn();
 				self.switchTurn = switchTurn;
 				self.getWinner= getWinner;
@@ -43,6 +42,7 @@
 				{circle: null }, {circle: null }, {circle: null },
 				{circle: null }, {circle: null }, {circle: null }
 				];
+
 				introSound.play();
 
 		// CREATES GAME ON FIREBASE //
@@ -63,104 +63,103 @@
 				self.game.spaces = 9;
 				self.game.playerOnePoints = 0;
 				self.game.playerTwoPoints = 0;
+				self.resetScore = resetScore;
 				self.game.$save(self.game);
 			}
 			// RESETS GAME BOARD AND SCORE FOR NEW GAME //
-				function newGame() {
-					self.currentPlayer = 0;
-					self.Winner = 0;
-					self.spaces = 9;
-					self.board = [
-					{circle: null }, {circle: null }, {circle: null },
-					{circle: null }, {circle: null }, {circle: null },
-					{circle: null }, {circle: null }, {circle: null }
-					];
-					resetSound.play();
-					self.game.$save(self.game);
-					}
-
-				function resetScore(){
-					self.Winner = 0;
-					self.spaces= 9;
-					self.playerOnePoints= 0;
-					self.playerTwoPoints= 0;
-					self.board = [
-					{circle: null }, {circle: null }, {circle: null },
-					{circle: null }, {circle: null }, {circle: null },
-					{circle: null }, {circle: null }, {circle: null }
-					];
-					resetSound.play();
-
+			function newGame() {
+				self.currentPlayer = 1;
+				self.Winner = 0;
+				self.spaces = 9;
+				self.board = [
+				{circle: null }, {circle: null }, {circle: null },
+				{circle: null }, {circle: null }, {circle: null },
+				{circle: null }, {circle: null }, {circle: null }
+				];
+				resetSound.play();
 				}
 
-				// SELECT TURN FUNCTION //
-				function selectTurn(){
-					self.currentPlayer = Math.ceil(Math.random() * 2);
-					}
+			function resetScore(){
+				self.Winner = 0;
+				self.spaces= 9;
+				self.playerOnePoints= 0;
+				self.playerTwoPoints= 0;
+				self.board = [
+				{circle: null }, {circle: null }, {circle: null },
+				{circle: null }, {circle: null }, {circle: null },
+				{circle: null }, {circle: null }, {circle: null }
+				];
+				resetSound.play();
 
-				// PLAYER MOVE FUNCTION //
-				function playerMove(index) {
-				  if (self.board[index].circle === null) {
-	  				// RANDOM SOUNDS PLAYS DURING PLAYERMOVE //
-	  				var randomSound=Math.floor(Math.random()*sounds.length);
-	  				var newSounds= new Audio(sounds[randomSound]);
-	  					newSounds.play();
-						if (self.currentPlayer === 1) {
-								self.board[index].circle = "X";
-								self.spaces--;
-								self.getWinner();
-								self.switchTurn();
-						}
-						else if (self.currentPlayer === 2) {
-								self.board[index].circle = "O";
-								self.spaces--;
-								self.getWinner();
-								self.switchTurn();
-						}
-					}
+			}
+
+			// SELECT TURN FUNCTION //
+			function selectTurn(){
+				self.currentPlayer = Math.ceil(Math.random() * 2);
 				}
 
-				// SWITCHES TURNS FOR PLAYERS //
-				function switchTurn(){
-						if (self.currentPlayer === 1) {
-								self.currentPlayer = 2;
-						}
-						else if (self.currentPlayer === 2){
-								self.currentPlayer = 1;
-						}
+			// PLAYER MOVE FUNCTION //
+			function playerMove(index) {
+			  if (self.board[index].circle === null) {
+  				// RANDOM SOUNDS PLAYS DURING PLAYERMOVE //
+  				var randomSound=Math.floor(Math.random()*sounds.length);
+  				var newSounds= new Audio(sounds[randomSound]);
+  					newSounds.play();
+					if (self.currentPlayer === 1) {
+							self.board[index].circle = "X";
+							self.spaces--;
+							self.getWinner();
+							self.switchTurn();
+					}
+					else if (self.currentPlayer === 2) {
+							self.board[index].circle = "O";
+							self.spaces--;
+							self.getWinner();
+							self.switchTurn();
+					}
 				}
-					// GAME LOGIC- DETERMINES GAME WINNER AND TIE, ADDS TO SCOREBOARD //
+			}
+
+			// SWITCHES TURNS FOR PLAYERS //
+			function switchTurn(){
+					if (self.currentPlayer === 1) {
+							self.currentPlayer = 2;
+					}
+					else if (self.currentPlayer === 2){
+							self.currentPlayer = 1;
+					}
+			}
+				// GAME LOGIC- DETERMINES GAME WINNER AND TIE, ADDS TO SCOREBOARD //
 
 				function getWinner() {
-						if (
+					if (
 
-						self.board[0].circle == 'X' && self.board[1].circle == 'X' && self.board[2].circle == 'X' ||
-				    self.board[3].circle == 'X' && self.board[4].circle == 'X' && self.board[5].circle == 'X' ||
-				    self.board[6].circle == 'X' && self.board[7].circle == 'X' && self.board[8].circle == 'X' ||
-				    self.board[0].circle == 'X' && self.board[4].circle == 'X' && self.board[8].circle == 'X' ||
-				    self.board[2].circle == 'X' && self.board[4].circle == 'X' && self.board[6].circle == 'X' ||
-			      self.board[0].circle == 'X' && self.board[3].circle == 'X' && self.board[6].circle == 'X' ||
-			   		self.board[1].circle == 'X' && self.board[4].circle == 'X' && self.board[7].circle == 'X' ||
-			      self.board[2].circle == 'X' && self.board[5].circle == 'X' && self.board[8].circle == 'X'
+					self.board[0].circle == 'X' && self.board[1].circle == 'X' && self.board[2].circle == 'X' ||
+			    self.board[3].circle == 'X' && self.board[4].circle == 'X' && self.board[5].circle == 'X' ||
+			    self.board[6].circle == 'X' && self.board[7].circle == 'X' && self.board[8].circle == 'X' ||
+			    self.board[0].circle == 'X' && self.board[4].circle == 'X' && self.board[8].circle == 'X' ||
+			    self.board[2].circle == 'X' && self.board[4].circle == 'X' && self.board[6].circle == 'X' ||
+		      self.board[0].circle == 'X' && self.board[3].circle == 'X' && self.board[6].circle == 'X' ||
+		   		self.board[1].circle == 'X' && self.board[4].circle == 'X' && self.board[7].circle == 'X' ||
+		      self.board[2].circle == 'X' && self.board[5].circle == 'X' && self.board[8].circle == 'X'
+					) {
 
-					 ){
-
-						self.Winner = "1";
-						self.playerOnePoints++;
-						console.log(self.Winner);
-						console.log(self.playerOnePoints);
-						winSound.play();
+					self.Winner = "1";
+					self.playerOnePoints++;
+					console.log(self.Winner);
+					console.log(self.playerOnePoints);
+					winSound.play();
 
 					} else if (
 
-				 	 self.board[0].circle == 'O' && self.board[1].circle == 'O' && self.board[2].circle == 'O' ||
-			     self.board[3].circle == 'O' && self.board[4].circle == 'O' && self.board[5].circle == 'O' ||
-			     self.board[6].circle == 'O' && self.board[7].circle == 'O' && self.board[8].circle == 'O' ||
-			     self.board[0].circle == 'O' && self.board[4].circle == 'O' && self.board[8].circle == 'O' ||
-			     self.board[2].circle == 'O' && self.board[4].circle == 'O' && self.board[6].circle == 'O' ||
-			     self.board[0].circle == 'O' && self.board[3].circle == 'O' && self.board[6].circle == 'O' ||
-			     self.board[1].circle == 'O' && self.board[4].circle == 'O' && self.board[7].circle == 'O' ||
-			     self.board[2].circle == 'O' && self.board[5].circle == 'O' && self.board[8].circle == 'O'
+				 	self.board[0].circle == 'O' && self.board[1].circle == 'O' && self.board[2].circle == 'O' ||
+			    self.board[3].circle == 'O' && self.board[4].circle == 'O' && self.board[5].circle == 'O' ||
+			    self.board[6].circle == 'O' && self.board[7].circle == 'O' && self.board[8].circle == 'O' ||
+			    self.board[0].circle == 'O' && self.board[4].circle == 'O' && self.board[8].circle == 'O' ||
+			    self.board[2].circle == 'O' && self.board[4].circle == 'O' && self.board[6].circle == 'O' ||
+			    self.board[0].circle == 'O' && self.board[3].circle == 'O' && self.board[6].circle == 'O' ||
+			    self.board[1].circle == 'O' && self.board[4].circle == 'O' && self.board[7].circle == 'O' ||
+			    self.board[2].circle == 'O' && self.board[5].circle == 'O' && self.board[8].circle == 'O'
 
 					) {
 					self.Winner = "2";
@@ -180,3 +179,7 @@
 			}
 		}
 })();
+
+
+
+
